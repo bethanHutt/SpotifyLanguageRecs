5a5ee858d0f5ec2e8f6c34429cc01ca4c25007ec
bethanhutt <bethan157@hotmail.com>
2020-10-09 17:45:38 +0100

    wip

--- app/tests/test_spotifychartsrequests.py
+++ app/tests/test_spotifychartsrequests.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pytest
+import requests
 
 ROOTDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path = [ROOTDIR] + sys.path
@@ -14,6 +15,7 @@ from spotify import spotify_charts_requests as spotcharts
 def chartrequester():
     chart_requester = spotcharts.ChartRequester(
         url=settings.CHARTS_URL,
+        type='regional',
         region='jp',
         frequency='weekly',
         date='latest')
@@ -32,6 +34,17 @@ def test_chart_requester_has_url_attr(chartrequester):
 @pytest.mark.parametrize('attrs', ['jp', 'weekly', 'latest'])
 def test_attrs_embedded_in_url(attrs, chartrequester):
     assert attrs in chartrequester.url
+
+
+def test_make_request_returns_response_object(chartrequester):
+    response = chartrequester.make_request()
+    assert isinstance(response, requests.models.Response)
+    assert response.status_code == 200
+
+
+def test_make_request_returns_good_response(chartrequester):
+    response = chartrequester.make_request()
+    assert response.status_code == 200
 
 
 if __name__ == '__main__':
