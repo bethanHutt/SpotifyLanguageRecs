b868ea3a42de55bfd159292015025ab7bae03030
bethanhutt <bethan157@hotmail.com>
2020-10-13 18:32:39 +0100

    get tracks lists artists

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import jsonify
+from flask import render_template
 
 from spotify import chart_data_handler
 from spotify import chart_requester
@@ -11,13 +12,7 @@ app.config['DEBUG'] = True
 app.config['FLASK_ENV'] = 'development'
 
 
-@ app.route('/')
-def home():
-    return 'Welcome to the Spotify Language Finder'
-
-
-@ app.route('/get_tracks')
-def get_tracks():
+def get_artists():
     cr = chart_requester.ChartRequester(
         url=settings.CHARTS_URL,
         type='regional',
@@ -29,7 +24,19 @@ def get_tracks():
     dh = chart_data_handler.DataHandler()
     dh.append_data(csv)
 
-    return dh.song_data.to_json()
+    return dh.song_data['Artist'].to_dict()
+
+
+@ app.route('/')
+@ app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@ app.route('/get_tracks')
+def get_tracks():
+    artists = list(set(get_artists().values()))
+    return render_template('get_tracks.html', artists=artists)
 
 
 if __name__ == '__main__':
