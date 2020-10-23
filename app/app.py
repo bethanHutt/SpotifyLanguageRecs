0cccfbc72b665de4919c536af8c6aa329e6d8f46
bethanhutt <bethan157@hotmail.com>
2020-10-23 18:39:19 +0100

    get tracks working with country name

--- app/app.py
+++ app/app.py
@@ -16,11 +16,11 @@ app.config['DEBUG'] = True
 app.config['FLASK_ENV'] = 'development'
 
 
-def get_artists():
+def get_artists(country_code):
     cr = chart_requester.ChartRequester(
         url=settings.CHARTS_URL,
         type='regional',
-        region='jp',
+        region=country_code,
         frequency='weekly',
         date='latest')
     csv = cr.make_request()
@@ -43,9 +43,16 @@ def home():
     return render_template('home.html', countries=countries)
 
 
-@ app.route('/get_tracks')
-def get_tracks():
-    artists = list(set(get_artists().values()))
+@ app.route('/get_tracks/<string:country>')
+def get_tracks(country):
+    script_path = os.path.dirname(__file__)
+    json_path = os.path.join(script_path, 'languages.json')
+    with open(json_path) as json_file:
+        json_data = json.load(json_file)
+
+    country_code = json_data[country.title()]
+    artists = list(set(get_artists(country_code=country_code).values()))
+
     return render_template('get_tracks.html', artists=artists, title='Tracks')
 
 
