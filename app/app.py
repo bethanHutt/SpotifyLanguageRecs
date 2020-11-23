263ef4d381128e169451827e909ecf7c0e3a5f2c
bethanhutt <bethan157@hotmail.com>
2020-11-23 17:00:11 +0000

    changed name

--- app/app.py
+++ app/app.py
@@ -2,8 +2,10 @@ import os
 import json
 
 from flask import Flask
+from flask import request
 from flask import jsonify
 from flask import url_for
+from flask import redirect
 from flask import render_template
 
 from spotify import chart_data_handler
@@ -31,27 +33,33 @@ def get_artists(country_code):
     return dh.song_data['Artist'].to_dict()
 
 
-@ app.route('/')
-@ app.route('/home')
+@ app.route('/', methods=['GET', 'POST'])
+@ app.route('/home', methods=['GET', 'POST'])
 def home():
+    if request.method == 'POST':
+        country = request.args.get('country')
+
+        script_path = os.path.dirname(__file__)
+        json_path = os.path.join(script_path, 'languages.json')
+
+        with open(json_path) as json_file:
+            json_data = json.load(json_file)
+
+        country_code = json_data[country.title()]
+        return redirect(url_for('get_tracks', country=country_code))
+
     script_path = os.path.dirname(__file__)
     json_path = os.path.join(script_path, 'languages.json')
     with open(json_path) as json_file:
-        json_data = json.load(json_file)
+        countries = json.load(json_file)
 
-    countries = list(json_data.keys())
     return render_template('home.html', countries=countries)
 
 
 @ app.route('/get_tracks/<string:country>')
 def get_tracks(country):
-    script_path = os.path.dirname(__file__)
-    json_path = os.path.join(script_path, 'languages.json')
-    with open(json_path) as json_file:
-        json_data = json.load(json_file)
 
-    country_code = json_data[country.title()]
-    artists = list(set(get_artists(country_code=country_code).values()))
+    artists = list(set(get_artists(country_code=country).values()))
 
     return render_template('get_tracks.html', artists=artists, title='Tracks')
 
